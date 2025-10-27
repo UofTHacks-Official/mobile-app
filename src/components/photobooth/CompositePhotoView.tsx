eb052719bb7a647cb4a545bc227183a56c1c4d74
mzhang055 <mzhang055@gmail.com>
2025-10-26 20:31:58 -0400

    add seconds on time label

--- src/components/photobooth/CompositePhotoView.tsx
+++ src/components/photobooth/CompositePhotoView.tsx
@@ -25,6 +25,7 @@ export default function CompositePhotoView({
     ? timestamp.toLocaleTimeString("en-US", {
         hour: "numeric",
         minute: "2-digit",
+        second: "2-digit",
         hour12: true,
       })
     : "";
