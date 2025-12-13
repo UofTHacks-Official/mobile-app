02735a7773bf1263d5200ef0165e56e4b06efaa1
mzhang055 <mzhang055@gmail.com>
2025-12-13 17:51:16 -0500

    update app version

--- app.config.js
+++ app.config.js
@@ -5,7 +5,7 @@ export default ({ config }) => {
     ...config,
     name: "uoft-hacks",
     slug: "uoft-hacks",
-    version: "1.0.0",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "native",
